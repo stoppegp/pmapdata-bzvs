f14ed0027a0a6609b3d8595a5f975b964ee83c43
Martin Stoppler <github@ma.stoppe-gp.de>
2017-05-05 08:31:34 +0200

    von pmap-Repo übernommen

---
+++ config.inc.php
@@ -0,0 +1,4 @@
+<?php
+define ("PMAP_TITLE",  "Piratenpartei Bezirk Stuttgart");
+define("PMAP_DESC", "Auf dieser Seite findest du, wo und wann im Bezirk Stuttgart überall Treffen der Piraten stattfinden.");
+?>
